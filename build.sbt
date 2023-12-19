35b4cea3dd1d1bf56b362cd9e93b39069c9a18ae
Sébastien Doeraene <sjrdoeraene@gmail.com>
2023-12-19 18:03:53 +0100

    Upgrade to tasty-mima v1.1.0.

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 
 // Must stay in sync with TastyMiMaPlugin.TastyMiMaVersion
-val TastyMiMaVersion = "1.0.0"
+val TastyMiMaVersion = "1.1.0"
 
 inThisBuild(Def.settings(
   crossScalaVersions := Seq("2.12.17"),
--- sbt-tasty-mima/src/main/scala/sbttastymima/TastyMiMaPlugin.scala
+++ sbt-tasty-mima/src/main/scala/sbttastymima/TastyMiMaPlugin.scala
@@ -9,7 +9,7 @@ import sbt.plugins.JvmPlugin
 
 object TastyMiMaPlugin extends AutoPlugin {
   // Must stay in sync with TastyMiMaVersion in build.sbt
-  private val TastyMiMaVersion = "1.0.0"
+  private val TastyMiMaVersion = "1.1.0"
 
   object autoImport {
     val tastyMiMaPreviousArtifacts: SettingKey[Set[ModuleID]] =
